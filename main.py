1a1d8316c881fe544ac292a6b46b57df1f5e158e
baashai <106022699+baashai@users.noreply.github.com>
2023-06-06 09:27:22 +0530

    Update main.py
    commited

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ for x in range(1, 10):
   print(x)
 for item in ['x', 'y', 'z']:
   print(item)
+for item in ['x', 'y', 'z']:
+  print(item)
+for item in ['x', 'y', 'z']:
+  print(item)
+for item in ['x', 'y', 'z']:
+  print(item)
+  
